f71f51c7b70ac7629fb08499e57f9bdbe7f25308
Naman Bhatt <54282392+crazycanoon@users.noreply.github.com>
2023-06-05 06:50:20 +0530

    Add files via upload

---
+++ NSP/Fees Structure/feesstructure.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style2.css" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">
+	</script>
+    <script src="Login/jquery-3.7.0.min.js"></script>
+  <title>DBMS</title>
+</head>
+
+</style>
+<body>
+   <!-- <img src="images/download.jpg"> -->
+  <section>
+    <!--for demo wrap-->
+    <h1>Courses & Fee</h1>
+    <?php
+
+// Database connection details
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dbms";
+
+// Create a connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Retrieve data from the "transport_fees" table
+$sql = "SELECT sno, courses, fees FROM courses_fees";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Display search input field
+    echo '<center><input type="text" id="searchInput" placeholder="Search..." /></center>';
+
+    // Create the table header
+    echo "<table>";
+    echo "<tr><th>Sno</th><th>Course </th><th>Fees</th></tr>";
+
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row["sno"] . "</td>";
+        echo "<td>" . $row["courses"] . "</td>";
+        echo "<td>" . $row["fees"] . "</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+} else {
+    echo "No records found.";
+}
+
+// Close the connection
+$conn->close();
+
+?>
+
+
+<script>
+    $(document).ready(function() {
+        // Capture keyup event on the search input
+        $("#searchInput").on("keyup", function() {
+            var value = $(this).val().toLowerCase();
+
+            // Filter the table rows based on the search term and highlight the matching results
+            $("table tr").filter(function() {
+                var rowText = $(this).text().toLowerCase();
+                var matchIndex = rowText.indexOf(value);
+                var matchLength = value.length;
+
+                // Remove previous highlighting
+                $(this).find("td").removeClass("highlight");
+
+                if (matchIndex > -1) {
+                    // Highlight the matching part of the row
+                    var highlightedText = rowText.substr(matchIndex, matchLength);
+                    var regex = new RegExp(highlightedText, "gi");
+                    var highlightedRowText = rowText.replace(regex, '<span class="highlight">' + highlightedText + '</span>');
+                    $(this).html($(this).html().replace(rowText, highlightedRowText));
+                }
+
+                $(this).toggle(rowText.indexOf(value) > -1);
+            });
+        });
+    });
+</script>    
+</body>
+</html>
---
+++ NSP/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Main Page</title>
+	<link rel="stylesheet" href="styles.css">
+	<script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+      <script src="https://kit.fontawesome.com/c21410e9fb.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<div class="wrapper">
+    <div class="sidebar">
+        <h2>PIET</h2>
+        <ul>
+            <li><a href="index.php"><i class="fas fa-home"></i>Home</a></li>
+           <!-- <li><a href="map.html"><i class="fas fa-map-pin"></i>BlockR</a></li> -->
+            <li><a href="#"><i class="fas fa-address-card"></i>Transcation</a></li>
+               <ul>
+                  <li><a href="https://fee.poornima.edu.in/"><i class="fas fa-project-arrow"></i> Pay Fees</a></li>
+                  <li><a href="Fees Structure/feesstructure.php"><i class="fas fa-project-arrow"></i>Courses & Fees</a></li>
+               </ul>
+            <li><a href="#"><i class="fas fa-blog"></i>About Us</a></li>
+            <li><a href="Contact us/contactus.php"><i class="fas fa-blo"></i>Contact Us</a></li>
+            <li><a href="Faculty detail/facultydetail.php"><i class="fas fa-blo"></i>Faculty Details</a></li>
+        </ul> 
+        <div class="social_media">
+          <a href="#"><i class="fab fa-facebook-f"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+      </div>
+    </div>
+    <div class="main_content">
+        <div class="header">Welcome!! </div>  
+        <div class="info">
+          <h1 align="center"> ONLINE COURSE REGISTRATION SYSTEM</h1>
+     </div>
+    </div>
+</div>
+<div class="insideimg">
+ <center><img src="img/download1.jpg" width="450" align="center"></center>
+ <div class="bottom" ><p>An authority offers some courses and notices the students. The processes are –
+
+<ul>
+<li><i class="fa-solid fa-arrow-right"></i> Authority offers courses.</li>
+<li><i class="fa-solid fa-arrow-right"></i> Notice the students.</li>
+<li><i class="fa-solid fa-arrow-right"></i> Student collect registration form.</li>
+<li><i class="fa-solid fa-arrow-right"></i> Students collect accounts clearance.</li>
+<li><i class="fa-solid fa-arrow-right"></i> Students submit registration from to the authority after fill up.</li>
+<li><i class="fa-solid fa-arrow-right"></i> Authority registers student & Provide's Login .</li>
+</ul></p></div>
+</div>
+ 
+</div>
+
+</body>
+</html>
